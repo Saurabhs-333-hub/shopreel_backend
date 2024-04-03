9822a227ea87bd55c1a5687ca715216aeee5199d
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2024-04-03 19:49:03 +0530

    commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+const routes = require('./apis/routes');
+require('dotenv').config(
+    {
+        path: './.env'
+    }
+)
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api/', routes);
+
+app.use((req, res, next) => {
+    if (req.url==='/') {
+        res.redirect('/api');
+        next();
+    }
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}!`);
+});
+
+module.exports=app;
