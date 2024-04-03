b0588d75b7a82a8070792dc785dff08667ccb878
Saurabh Soni <sonisaurabhsoni333@gmail.com>
2024-04-03 19:56:01 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3000;
-const routes = require('./apis/routes');
+const routes = require('../apis/routes');
 require('dotenv').config(
     {
         path: './.env'
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/', routes);
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use((req, res, next) => {
     if (req.url==='/') {
         res.redirect('/api');
